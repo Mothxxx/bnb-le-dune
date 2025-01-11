ec9de07e570f2ae326378e06b7187f45b32a3611
Mothxxx <fmaiomascio@gmail.com>
2025-01-11 19:45:45 +0100

    release 1.0

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from re import X
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
+    
 @app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
@@ -14,6 +16,14 @@ def camera1():
 @app.route('/camera2',  methods=["GET"])
 def camera2():
     return render_template('camera2.html')
+
+@app.route('/camere',  methods=["GET"])
+def camere():
+    return render_template('camere.html')
+
+@app.route('/privacy_policy', methods=["GET"])
+def privacy_policy():
+    return render_template('privacy_policy.html')  # Assicurati che il file HTML sia salvato in templates/
 
 
 if __name__ == '__main__':
